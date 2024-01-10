0c4b32c79d05d00596b5cf789702a698864e2356
niniarob <robakidzenino89@gmail.com>
2024-01-10 21:49:15 +0400

    add particlescontainer

--- pages/index.js
+++ pages/index.js
@@ -60,16 +60,17 @@ const Home = () => {
         {/* bg image */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
       </div>
+      
       <div>
         {/* particles */}
-        <ParticlesContainer/>
+        <ParticlesContainer />
         {/* avatar image */}
         <motion.div
           variants={fadeIn("up", 0.5)}
           initial="hidden"
           animate="show"
           exit="hidden"
-          transition={{duration:1, ease: 'easeInOut'}}
+          transition={{ duration: 1, ease: "easeInOut" }}
           className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
         >
           <Avatar />
